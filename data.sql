1b6db760b60177bedfc915bc2c016da6346d8514
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-01-07 10:32:08 +0300

    chor: add script for updates data

--- data.sql
+++ data.sql
@@ -11,3 +11,20 @@ insert into ingredient (name, price, category, id_dish) values
 ('Poulet', 4500.00, 'ANIMAL', 2),
 ('Chocolat', 3000.00, 'OTHER', 4),
 ('Beurre', 2500.00, 'DAIRY', 4);
+
+
+UPDATE Ingredient
+SET required_quantity = 1
+WHERE name = 'Laitue';
+
+UPDATE Ingredient
+SET required_quantity = 2
+WHERE name = 'Tomate';
+
+UPDATE Ingredient
+SET required_quantity = 0.5
+WHERE name = 'Poulet';
+
+UPDATE Ingredient
+SET required_quantity = NULL
+WHERE name IN ('Chocolat', 'Beurre');
